053f3e95d9b2db7a7685ddc7e32526e8e799b542
Serhat Serçe <serhat2549@outlook.com>
2026-04-03 16:42:50 +0300

    Initial setup: Spring Boot project, PostgreSQL connection, Account entity

---
+++ src/test/java/com/serhat/bankingtransactionapi/BankingTransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhat.bankingtransactionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
